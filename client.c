2b61487c7d26cd1d382c2f7ba9fa175a9994b0fe
Jakub Misiaszek <jmisiaszek3@gmail.com>
2024-06-06 00:54:26 +0200

    initial setup of a project

---
+++ client.c
@@ -0,0 +1,18 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <errno.h>
+#include <inttypes.h>
+#include <limits.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#include "err.h"
+#include "common.h"
+
+int main(int argc, char *argv[]) {
+    
+}
